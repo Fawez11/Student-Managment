adb68b3d9b4a19273a1e839c490a22518f3dfc12
Fawez11 <fawezferjaoui11@gmail.com>
2022-06-29 17:10:17 +0200

    post get

--- schema.sql
+++ schema.sql
@@ -1,3 +1,14 @@
 DROP DATABASE IF EXISTS managestudent;
 CREATE DATABASE managestudent;
 USE managestudent;
+
+DROP TABLE IF EXISTS student;
+
+CREATE TABLE student (
+  id INTEGER UNIQUE AUTO_INCREMENT,
+  firstName VARCHAR(20),
+  lastName VARCHAR(140),
+  Age VARCHAR(140),
+  PRIMARY KEY (id)
+)
+INSERT INTO student (firstName,lastName,Age) VALUES("fawez","ferjaoui",28);
